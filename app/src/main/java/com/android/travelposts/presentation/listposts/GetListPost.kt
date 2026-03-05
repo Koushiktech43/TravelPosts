b02ab98db04e64f1dc2b10d6bd8787f46951caef
Koushik <koushiknus@gmail.com>
2026-03-04 21:43:09 -0600

    New : Added Search

--- app/src/main/java/com/android/travelposts/presentation/listposts/GetListPost.kt
+++ app/src/main/java/com/android/travelposts/presentation/listposts/GetListPost.kt
@@ -1,6 +1,7 @@
 package com.android.travelposts.presentation.listposts
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,43 +9,65 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.android.travelposts.data.remote.TravelPostDto
 import com.android.travelposts.presentation.common.UiState
 
 
 @Composable
-fun GetListPostScreen(viewModel: GetTravelPostViewModel){
+fun GetListPostScreen(viewModel: GetTravelPostViewModel) {
 
-    val travelPostState by viewModel.uiSate.collectAsStateWithLifecycle()
+    val travelPostState by viewModel.uiState.collectAsStateWithLifecycle()
+    val searchQuery by viewModel.searchQuery.collectAsStateWithLifecycle()
+    val filteredList by viewModel.filteredList.collectAsStateWithLifecycle()
 
     Column(
-       modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
-        when(travelPostState){
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = {viewModel.updateSearchQuery(it)},
+            modifier = Modifier.fillMaxWidth().padding(24.dp),
+            placeholder = {Text("Search...")},
+            singleLine = true
+        )
+        when (travelPostState) {
             is UiState.Error -> TODO()
-            UiState.Initial , UiState.Loading -> {
-                CircularProgressIndicator()
+            UiState.Initial, UiState.Loading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+
             }
+
             is UiState.Success -> {
                 LazyColumn(
-                    modifier = Modifier.fillMaxWidth().padding(16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
                     verticalArrangement = Arrangement.Center
 
                 ) {
-                    val listOfData = (travelPostState as UiState.Success<TravelPostDto>).data.data.children
-                    items(listOfData){ item->
-                        Text(text =item.data.name)
+                    items(filteredList) { item ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(text = item.data.name)
+                        }
                     }
                 }
             }
         }
-
     }
-
 }
